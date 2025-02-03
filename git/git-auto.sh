00af67f4c7437107beb2afb7e853a8611a518b8c
Georgios Drivas <drivasgeorgios0@gmail.com>
2025-02-03 18:23:17 +0200

    git-auto script: Add repo url as argument

--- git/git-auto.sh
+++ git/git-auto.sh
@@ -5,13 +5,17 @@ if [ ! -d ".git" ]; then
     exit 1
 fi
 
-if [ -z "$1" ]; then
-    echo "Usage: git-quick \"commit message\""
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: git-quick <repository_url> \"commit message\""
     exit 1
 fi
 
+git init
+
+git remote add origin "$1"
+
 git add .
-git commit -m "$1"
-git push
+git commit -m "$2"
+git push -u origin master
 
 echo "Changes pushed successfully!"
